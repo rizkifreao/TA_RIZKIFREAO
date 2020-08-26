d304d8fce5efddd3ee42bb2f4f881a77239033de
Rizkifreao <rizkipebrianto96@gmail.com>
2020-08-26 14:36:24 +0700

    update

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ const app = express()
 const mqtt_handler = require('./mqtt_handler')
 const wa = require('./wa_handler')
 
+const serverOption = {
+  headless: true,
+  qrTimeout: 40,
+  authTimeout: 40,
+  autoRefresh: true,
+  qrRefreshS: 15,
+  devtools: false,
+  chromiumArgs: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]
+}
+
+// const opsys = process.platform;
+// if (opsys == "win32" || opsys == "win64") {
+//   serverOption['executablePath'] = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+// } else if (opsys == "linux") {
+//   serverOption['browserRevision'] = '737027';
+// } else if (opsys == "darwin") {
+//   serverOption['executablePath'] = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+// }
+
 app.use(express.json())
 const PORT = process.env.PORT;
 
@@ -21,9 +43,7 @@ const start = async (client) => {
   })
 }
 
-create({
-  sessionId: 'session1'
-}).then(async client => await start(client))
+create(serverOption).then(async client => await start(client))
   .catch(e => {
     console.log('Error', e.message);
   });
