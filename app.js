7da2503ab562721cbe0231198fad644c7ae018de
Rizkifreao <rizkipebrianto96@gmail.com>
2020-08-26 14:39:12 +0700

    update app

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ const serverOption = {
   ]
 }
 
-const opsys = process.platform;
-if (opsys == "win32" || opsys == "win64") {
-  serverOption['executablePath'] = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
-} else if (opsys == "linux") {
-  serverOption['browserRevision'] = '737027';
-} else if (opsys == "darwin") {
-  serverOption['executablePath'] = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-}
+// const opsys = process.platform;
+// if (opsys == "win32" || opsys == "win64") {
+//   serverOption['executablePath'] = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
+// } else if (opsys == "linux") {
+//   serverOption['browserRevision'] = '737027';
+// } else if (opsys == "darwin") {
+//   serverOption['executablePath'] = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+// }
 
 app.use(express.json())
 const PORT = process.env.PORT;
